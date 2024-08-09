b0978160ec27fe2efd6a8c5f0672c15fc54b6182
Srivatsan <vatsan1993@gmail.com>
2024-08-09 17:56:49 +0530

    project finished

--- fetch-requests.js
+++ fetch-requests.js
@@ -6,21 +6,23 @@ Paste your code for fetch requests here once you finish each task.
 /* =============== 1. Print the status code of the response =============== */
 
 // Your code here
-
-
+let url = '/products';
+let response = fetch(url);
+console.log(response.status);
 
 /* ====== 2. Print true if the status of the response was successful ====== */
 
 // Your code here
-
-
+console.log(response.ok);
 
 /* =================== 3. Print the Content-Type Header =================== */
 
 // Your code here
-
-
+console.log(response.headers.get('Content-Type'));
 
 /* ============== 4. Print the body of the response as text =============== */
 
 // Your code here
+response.text().then((data) => {
+  console.log(data);
+});
